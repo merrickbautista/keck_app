befade19e0f6861adce9980fc18999b0e51d15a5
Merrick Bautista <merrickbautista@gmail.com>
2015-04-18 19:41:02 -0700

    Added pagination

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,8 +7,19 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 question = Question.create(content: "The presynpatic cell reuptakes choline by ___________.", subject: "Physiology")
-
 question.answers.create(content: "diffusion")
 question.answers.create(content: "paired movement with Na+", correct: true)
 question.answers.create(content: "paired movement with K+")
 question.answers.create(content: "active transportation")
+
+question = Question.create(content: "What is the physiology of myasthenia gravis?", subject: "Physiology")
+question.answers.create(content: "The physiology is unknown.")
+question.answers.create(content: "Nicotine acetylcholine receptors become defective due to environmental factors.")
+question.answers.create(content: "SNP in gene causes mutation in nicotine acetylcholine receptors.")
+question.answers.create(content: "Antibodies lead to less nicotine acetylcholine receptors.", correct: true)
+
+question = Question.create(content: "Which ion gates are influenced in EPSP?", subject: "Physiology")
+question.answers.create(content: "Na+")
+question.answers.create(content: "K+")
+question.answers.create(content: "Both", correct: true)
+question.answers.create(content: "Neither")
