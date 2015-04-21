861fa4e62c7c51670665594983b917fb10d1f415
Merrick Bautista <merrickbautista@gmail.com>
2015-04-20 20:10:19 -0700

    Need to fix review issue

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   resources :exams
   get 'pre_show', to: 'questions#pre_show'
   get 'finished', to: 'static_pages#finished'
+  get 'review', to: 'questions#review'
   root to: "static_pages#home"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
